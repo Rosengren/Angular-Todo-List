dc70e00f1af181056c47e82da452c3873f0d264b
Rosengren <krosen@amazon.com>
2016-07-25 11:17:33 -0700

    Added action buttons and basic controller to the index page

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-angular.module('todoListApp', []);
+angular.module('todoListApp', [])
+.controller('MainController', function($scope) {
+  $scope.helloWorld = function() {
+    console.log("Hello there! This is the hello world controller function in the mainCtrl");
+  }
+});
