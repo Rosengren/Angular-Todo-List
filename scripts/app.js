deb1d31e1270f11e32e45ad86030edcad2859942
Rosengren <krosen@amazon.com>
2016-07-25 13:04:07 -0700

    added a basic service and mock data

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,26 @@
 angular.module('todoListApp', [])
-.controller('MainController', function($scope) {
-  $scope.helloWorld = function() {
-    console.log("Hello there! This is the hello world controller function in the mainCtrl");
+.controller('MainController', function($scope, dataService) {
+
+  $scope.helloConsole = dataService.helloConsole;
+
+  $scope.learningNgChange = function() {
+    console.log("An input changed");
+  };
+
+  dataService.getTodos(function(response) {
+    console.log(response);
+    $scope.todos = response.data;
+});
+
+})
+.service('dataService', function($http) {
+
+  this.helloConsole = function() {
+    console.log("This is the hello console service.");
+  }
+
+  this.getTodos = function(callback) {
+    $http.get('mock/todos.json')
+      .then(callback);
   }
 });
